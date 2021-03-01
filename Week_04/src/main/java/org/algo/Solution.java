7795d3678f2365570325ad3bc8f1897faa172db8
yiyimi <yiyimi@users.noreply.github.com>
2021-03-01 14:54:58 +0800

    Update Solution.java

--- Week_04/src/main/java/org/algo/Solution.java
+++ Week_04/src/main/java/org/algo/Solution.java
@@ -115,6 +115,35 @@ public class Solution {
         return -1;
     }
 
+    /**
+     * @Description: 搜索旋转排序数据
+     * T(n)=O(logn); S(n)=O(1). 
+     * @Author: yiyimi
+     * @Date: 2021/3/1 0001
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target == nums[mid]) return mid;
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 
 
 }
